431f825e07702777856a213906ad4049affff8a9
rizqi-reza <rizqi.valhevi@bhinneka.com>
2020-10-21 13:41:02 +0700

    update 21/10/2020

--- src/components/molecules/footer/component.tsx
+++ src/components/molecules/footer/component.tsx
@@ -6,14 +6,15 @@ export const FooterComponent = (props: any) => {
       <div className="container">
         <div className="hm-footer-copyright text-center">
           <p>
-            &copy; copyright Rizqi Reza Valhevi. design and developed by <a href="#">rizqi-reza</a>
+            &copy; copyright Rizqi Reza Valhevi. design and developed by{' '}
+            <a href="https://github.com/rizqi-reza">rizqi-reza</a>
           </p>
         </div>
       </div>
 
       <div id="scroll-Top">
         <div className="return-to-top">
-          <i className="fa fa-angle-up " id="scroll-top" />
+          <i className="fa fa-angle-up" id="scroll-top" />
         </div>
       </div>
     </footer>
