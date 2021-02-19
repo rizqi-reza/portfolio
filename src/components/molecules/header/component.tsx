34b00770542600c47b6b393c06377ecd1fd0ef6c
rizqi-reza <rizqi.valhevi@bhinneka.com>
2021-02-20 06:32:13 +0700

    update 20/02/2021

--- src/components/molecules/header/component.tsx
+++ src/components/molecules/header/component.tsx
@@ -1,6 +1,7 @@
 import { INavigation } from 'interface/ipage';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import Scrollspy from 'react-scrollspy';
 
 interface IProps {
   title: string;
@@ -19,18 +20,35 @@ export const HeaderComponent = (props: IProps) => {
             <Navbar.Brand href="#section-1">{title}</Navbar.Brand>
             {dataSource && (
               <Navbar.Collapse id="navbar-menu" className="menu-ui-design">
-                <Nav className="ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
+                <Scrollspy
+                  componentTag="div"
+                  items={[
+                    'section-1',
+                    'section-2',
+                    'section-3',
+                    'section-4',
+                    'section-5',
+                    'section-6',
+                    'section-7',
+                  ]}
+                  currentClassName="active"
+                  className="ml-auto navbar-nav"
+                  offset={-100}
+                >
+                  <Nav.Link className="smooth-menu" href="#section-1" style={{ display: 'none' }}>
+                    Home
+                  </Nav.Link>
                   {dataSource.map((item: INavigation, index: number) => (
                     <Nav.Link
-                      className="smooth-menu"
                       key={`menu-${index}`}
+                      className="smooth-menu"
                       href={`#${item.url}`}
                       aria-label={`${item.title}-link`}
                     >
                       {item.title}
                     </Nav.Link>
                   ))}
-                </Nav>
+                </Scrollspy>
               </Navbar.Collapse>
             )}
           </Container>
