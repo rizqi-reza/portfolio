2a2a7e97c546bcd64c7e458dc6a19a827279ae36
rizqi-reza <rizqi.valhevi@bhinneka.com>
2021-04-11 17:06:25 +0700

    wedding 11/04/2021

--- src/pages/main/component.tsx
+++ src/pages/main/component.tsx
@@ -6,6 +6,7 @@ import IPage from 'interface/ipage';
 import { Fragment, useEffect, useState } from 'react';
 import React from 'react';
 import Favicon from 'react-favicon';
+import DocumentMeta from 'react-document-meta';
 
 export const MainPageComponent = (props: any) => {
   const [page, setPage] = useState({} as IPage);
@@ -28,9 +29,16 @@ export const MainPageComponent = (props: any) => {
     setLoader(false);
   };
 
+  const pageTitle = 'Rizqi Reza';
+  const meta = {
+    pageTitle,
+    description: `I'm a experienced Software Developer and System Analyst. Skilled in Frontend and Backend Development, System Analytical, and Databases.`,
+  };
+
   return (
     <Fragment>
       <Favicon url="https://drive.google.com/uc?export=view&id=1HFtSQHgkxGnq6iD3d5Q2ggoc8KwJOQco" />
+      <DocumentMeta {...meta} />
       <Header title={page.title} dataSource={page.navigationMenus} />
       {loader ? <Loader /> : <Fragment>{page && renderAllSections(page?.sections)}</Fragment>}
       <Footer />
